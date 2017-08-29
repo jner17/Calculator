096268b34d0afb7b85e5fdba3f2cf230c116fd82
Richard Robinson <r.jason.robinson12@gmail.com>
2017-08-29 16:14:00 -0700

    Added ability to multiply, divide, subract, add, and square two values.

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,23 @@
-# User is given prompt to enter formula
+user_input = ''
+while user_input not in ['q', 'quit', 'exit']:
+    # User is given prompt to enter formula
+    user_input = input('Enter a formula: ')
+
+    # Break the formula up, eg. 2x3, [2, 3]
+    if 'x' in user_input:
+        parts = user_input.split('x')
+        solution = float(parts[0]) * float(parts[1])
+    elif '/' in user_input:
+        parts = user_input.split('/')
+        solution = float(parts[0]) / float(parts[1])
+    elif '+' in user_input:
+        parts = user_input.split('+')
+        solution = float(parts[0]) + float(parts[1])
+    elif '-' in user_input:
+        parts = user_input.split('-')
+        solution = float(parts[0]) - float(parts[1])
+    elif '^' in user_input:
+        parts = user_input.split('^')
+        solution = float(parts[0]) ** float(parts[1])
+
+    print(solution)
